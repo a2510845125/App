aa7a5b5725395f9dc2d2f79a7e71532a51b8f06a
Administrator <l15666315421@163.com>
2020-08-20 16:18:45 +0800

    dao包

--- src/cn/appsys/dao/aa.java
+++ src/cn/appsys/dao/aa.java
@@ -2,4 +2,12 @@ package cn.appsys.dao;
 
 public class aa {
     private Integer a;
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
 }
